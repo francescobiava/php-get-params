82d226901064d09eab18e0a4fdb38005852f41b5
Francesco <franc.ns@gmail.com>
2019-12-06 14:44:27 +0100

    version 1

--- index.php
+++ index.php
@@ -5,8 +5,26 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
   <title>Document</title>
+  
+  <?php
+    
+    $password = $_GET['password'];
+    if ($password === 'boolean') {
+      $message = '<h1 style="color:green"> Accesso riuscito </h1>';
+    } else {
+      $message = '<h1 style="color:red"> Accesso negato </h1>';
+    }
+
+  ?>
+
 </head>
 <body>
+
+<?php 
+
+echo $message;
+
+?>
 
 </body>
 </html>
